cf817bcfbc6526754f6caa868eed1bbdfcdee0c2
anlegent <alegent@edenschool.fr>
2025-01-17 16:03:51 +0100

    commit3

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 const div1 = document.getElementById("div1");
 const div2 = document.getElementById("div2");
-const btn = document.getElementById("showartbtn");
+const btnnext = document.getElementById("showartbtn");
+const btnprev = document.getElementById("closeartbtn");
+
+btnnext.addEventListener("click", () => {
+  div1.style.display = "none";
+  div2.style.display = "box";
+});
